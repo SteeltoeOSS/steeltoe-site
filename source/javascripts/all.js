a34e8784f901090bb87426637caf61c6c241cda3
Matt Rothenberg <mattrothenberg@gmail.com>
2016-10-27 20:45:03 -0400

    Add some interaction design love

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   var codeSnippets = $('.codeSnippet');
+
   codeSnippets.each(function(index, cs) {
     hljs.highlightBlock(cs);
   });
+
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles', 'particle-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
 });
